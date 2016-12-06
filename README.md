55996b88c2c9f11079da062d6835f7c7001e39db
laishanhai1040 <laishanhai1040@gmail.com>
2016-12-06 19:57:57 +0800

    2016/12/06 19:57

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calculator
+<br>
+# 程序运行的情况
+> $ calculator  
+> This is a calculator.  
+> Enter /h for help.  
+> Please enter sum(num1, num2, num3, ... ,numn), and press enter.  
--- calculator.c
+++ calculator.c
@@ -1,11 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <math.h>
 
-int main()
+#define _GNU_SIURSE
+
+#include <getopt.h>
+
+int main(int argc, char *argv[])
 {
+    int opt;
+    struct option longopts[] = {
+      {"initilalize", 0, NULL, 'i'},
+      {"file", 1, NULL, 'f'},
+      {"list", 0, NULL, 'l'},
+      {"restart", 0, NULL, 'r'},
+      {0, 0, 0, 0}
+    }
+
+    while((opt = getopt_long(argc, argv, ":if:lr", longopts, NULL)) != -1) {
+      switch(opt) {
+        case 'i':
+        case 'l':
+        case 'r':
+        printf("option: %c\n", opt);
+        break;
+        case 'f':
+        printf("filename: %s\n", optarg);
+        break;
+        case ':':
+        printf("option needs a value\n");
+        break;
+        case '?':
+        printf("unknown option: %c\n", optopt);
+        break;
+      }
+    }
+
+    for(; optind < argc; optind++)
+        printf("argument: %s\n", argv[opting]);
+
+
     printf("This is a calculator\n");
     printf("Enter /h for help.\n");
-    printf("Please enter sum(num1, num2, num3, ... , numn); ,and press enter.\n");: 
+    printf("Please enter sum(num1, num2, num3, ... , numn); ,and press enter.\n");:
     return 0;
 }
