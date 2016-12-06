2ca14999d6806d7383be3d6bd92fd6624b217436
laishanhai1040 <laishanhai1040@gmail.com>
2016-12-06 12:31:12 +0000

    2016/12/06 20:31

--- calculator.c
+++ calculator.c
@@ -44,5 +44,11 @@ int main(int argc, char *argv[])
     for(; optind < argc; optind++)
         printf("argument: %s\n", argv[optind]);
 
+    double sum, v;
+
+    sum = 0;
+    while(scanf("%lf", &v) == 1)
+        printf("\t%.2f\n", sum += v);
+
     return 0;
 }
